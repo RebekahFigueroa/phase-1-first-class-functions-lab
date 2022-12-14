cd6651785dc9264fa5561b38886ddfe9060544ef
Rebekah <figueroarebekah@gmail.com>
2022-12-14 15:25:37 -0600

    commit tree 2f352580d894103a85c682ccd7248e401f9a1be0

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+// const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(i) {
+  return function (fare) {
+    return fare * i;
+  };
+}
+
+function fareDoubler(fare) {
+  return fare * 2;
+}
+
+function fareTripler(fare) {
+  return fare * 3;
+}
+
+function selectDifferentDrivers(drivers, callback) {
+  return callback(drivers);
+}
